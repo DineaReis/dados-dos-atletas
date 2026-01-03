83b7443ac5d602cbef0567968ed4086c6d0555fc
Dinea <dineareis11.m@gmail.com>
2026-01-03 18:19:41 -0300

    Enviar código do projeto

---
+++ script.js
@@ -0,0 +1,74 @@
+class Atleta {
+  constructor(nome, idade, peso, altura, notas) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+    this.notas = notas;
+  }
+
+  // 1. Calcula a categoria
+  calculaCategoria() {
+    if (this.idade >= 9 && this.idade <= 11) {
+      return "Infantil";
+    } else if (this.idade >= 12 && this.idade <= 13) {
+      return "Juvenil";
+    } else if (this.idade >= 14 && this.idade <= 15) {
+      return "Intermediário";
+    } else if (this.idade >= 16 && this.idade <= 30) {
+      return "Adulto";
+    } else {
+      return "Sem categoria";
+    }
+  }
+
+  // 2. Calcula o IMC
+  calculaIMC() {
+    return this.peso / (this.altura * this.altura);
+  }
+
+  // 3. Calcula a média válida
+  calculaMediaValida() {
+    let notasOrdenadas = [...this.notas].sort((a, b) => a - b);
+
+    // Remove a menor e a maior nota
+    notasOrdenadas.shift();
+    notasOrdenadas.pop();
+
+    let soma = notasOrdenadas.reduce((total, nota) => total + nota, 0);
+    return soma / notasOrdenadas.length;
+  }
+
+  // Métodos getters
+  obtemNomeAtleta() {
+    return this.nome;
+  }
+
+  obtemIdadeAtleta() {
+    return this.idade;
+  }
+
+  obtemPesoAtleta() {
+    return this.peso;
+  }
+
+  obtemAlturaAtleta() {
+    return this.altura;
+  }
+
+  obtemNotasAtleta() {
+    return this.notas;
+  }
+
+  obtemCategoria() {
+    return this.calculaCategoria();
+  }
+
+  obtemIMC() {
+    return this.calculaIMC();
+  }
+
+  obtemMediaValida() {
+    return this.calculaMediaValida();
+  }
+}
