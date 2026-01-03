44ea9b69ed6f0aca30fb9c9a9315a199250ab953
Dinea <dineareis11.m@gmail.com>
2026-01-03 18:33:11 -0300

    Adiciona execução e exibição dos dados do atleta

--- script.js
+++ script.js
@@ -72,3 +72,21 @@ class Atleta {
     return this.calculaMediaValida();
   }
 }
+
+const atleta = new Atleta(
+  "Cesar Abascal",
+  30,
+  80,
+  1.70,
+  [10, 9.34, 8.42, 10, 7.88]
+);
+
+console.log("Nome:", atleta.obtemNomeAtleta());
+console.log("Idade:", atleta.obtemIdadeAtleta());
+console.log("Peso:", atleta.obtemPesoAtleta());
+console.log("Altura:", atleta.obtemAlturaAtleta());
+console.log("Notas:", atleta.obtemNotasAtleta().join(","));
+console.log("Categoria:", atleta.obtemCategoria());
+console.log("IMC:", atleta.obtemIMC());
+console.log("Média válida:", atleta.obtemMediaValida());
+
